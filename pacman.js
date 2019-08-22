40eec403622bba80a1f89eca21a88401fc9e9d63
Mark Jarvis <mtgjarvis@gmail.com>
2019-08-22 16:25:43 -0400

    completed minus the stretch

--- pacman.js
+++ pacman.js
@@ -1,10 +1,10 @@
 // Setup initial game stats
 let score = 0;
 let lives = 2;
+let powerPellets = 4;
 
 
 // Define your ghosts here
-
 const inky = {
   menuOption: '1',
   name: 'Inky',
@@ -12,6 +12,7 @@ const inky = {
   character: 'Shadow',
   edible: false
 };
+
 
 const blinky = {
   menuOption: '2',
@@ -36,6 +37,12 @@ const clyde = {
   character: 'Pokey',
   edible: false
 };
+
+// let ghosts =['inky', 'blinky', 'pinky', 'clyde']
+let ghosts = [inky, blinky, pinky, clyde]
+
+
+
 // replace this comment with your four ghosts setup as objects
 
 
@@ -44,6 +51,7 @@ function drawScreen() {
   clearScreen();
   setTimeout(() => {
     displayStats();
+    displayPowerPellets();
     displayMenu();
     displayPrompt();
   }, 10);
@@ -57,10 +65,24 @@ function displayStats() {
   console.log(`Score: ${score}     Lives: ${lives}`);
 }
 
+function displayPowerPellets() {
+  console.log(`\n\nPower-Pellets: ${powerPellets}`);
+}
+
+
 function displayMenu() {
   console.log('\n\nSelect Option:\n');  // each \n creates a new line
   console.log('(d) Eat Dot');
+  console.log('(p) Eat Power-Pellet');
   console.log('(q) Quit');
+  ghosts.forEach(function(ghost, i) {
+    i++
+    if (ghost.edible == false) {
+  console.log (`(${i}) Eat ${ghost.name} -- (inedible)`)
+    } else if (ghost.edible == true) {
+  console.log (`(${i}) Eat ${ghost.name} -- (edible)`)
+    }
+  })
 }
 
 function displayPrompt() {
@@ -75,6 +97,29 @@ function eatDot() {
   score += 10;
 }
 
+function eatPowerPellet() {
+  if (powerPellets > 0) {
+  console.log('\nYummm!');
+  score += 50;
+  powerPellets -= 1;
+    ghosts.forEach(ghost => {
+      ghost.edible = true;
+    });
+    }
+  }
+
+function eatGhost(index) {
+  let ghost = ghosts[index]
+  if (!ghost.edible) {
+    lives -= 1;
+    console.log(`\nPac-Man was killed by ${ghost.name} with the colour ${ghost.colour}`);
+    } else if (ghost.edible) {
+      score += 200;
+      console.log(`\nPac-Man ate ${ghost.name} whose colour was ${ghost.colour}`);
+      ghost.edible = false;
+    }
+  }
+
 
 // Process Player's Input
 function processInput(key) {
@@ -85,6 +130,25 @@ function processInput(key) {
       break;
     case 'd':
       eatDot();
+      break;
+    case 'p':
+        if (powerPellets === 0) {
+          console.log('\nNo Power-Pellets Left')
+        } else if (powerPellets > 0) {
+          eatPowerPellet();
+        }
+      break;
+    case '1':
+      eatGhost(0);
+      break;
+    case '2':
+      eatGhost(1);
+      break;
+    case '3':
+      eatGhost(2);
+      break;
+    case '4':
+      eatGhost(3);
       break;
     default:
       console.log('\nInvalid Command!');
@@ -109,7 +173,7 @@ drawScreen();
 stdin.on('data', (key) => {
   process.stdout.write(key);
   processInput(key);
-  setTimeout(drawScreen, 300); // The command prompt will flash a message for 300 milliseoncds before it re-draws the screen. You can adjust the 300 number to increase this.
+  setTimeout(drawScreen, 1000); // The command prompt will flash a message for 300 milliseoncds before it re-draws the screen. You can adjust the 300 number to increase this.
 });
 
 // Player Quits
